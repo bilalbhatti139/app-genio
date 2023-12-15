ce3d137796614061e815a3f413798241f5c1ea32
bilalbhatti139 <bilalbhatti139@gmail.com>
2023-12-15 18:52:29 +0500

    link-update
    link-update

--- src/Components/Informations.js
+++ src/Components/Informations.js
@@ -4,41 +4,37 @@ import RandomFarewellMessage from "./Random";
 
 const Informations = ({ json }) => {
   const { buttonsText } = usePresssedButtonsText();
-  // const buttonsData = [
-  //   { text: "Libros", borderRadius: 5, Color: "#5082c8", internal: "libros" },
-  //   {
-  //     text: "Películas",
-  //     borderRadius: 5,
-  //     Color: "#9E49C4",
-  //     internal: "coleccionismo-cine",
-  //   },
-  //   {
-  //     text: "Música",
-  //     borderRadius: 5,
-  //     Color: "#FFBD59",
-  //     internal: "musica-discos-vinilos",
-  //   },
-  //   {
-  //     text: "Juegos y Juguetes",
-  //     borderRadius: 5,
-  //     Color: "#D0A3CF",
-  //     internal: "juguetes",
-  //   },
-  //   {
-  //     text: "Cómics",
-  //     borderRadius: 5,
-  //     Color: "#7600A9",
-  //     internal: "tebeos-comics",
-  //   },
+  const buttonsData = [
+    { text: "Libros", borderRadius: 5, Color: "#5082c8", internal: "libros" },
+    {
+      text: "Películas",
+      borderRadius: 5,
+      Color: "#9E49C4",
+      internal: "coleccionismo-cine",
+    },
+    {
+      text: "Música",
+      borderRadius: 5,
+      Color: "#FFBD59",
+      internal: "musica-discos-vinilos",
+    },
+    {
+      text: "Juegos y Juguetes",
+      borderRadius: 5,
+      Color: "#D0A3CF",
+      internal: "juguetes",
+    },
+    {
+      text: "Cómics",
+      borderRadius: 5,
+      Color: "#7600A9",
+      internal: "tebeos-comics",
+    },
+  ];
+  const isButtonValid = buttonsData.some(
+    (button) => button.internal === buttonsText
+  );
 
-  // ];
-  // let text='';
-  // const isButtonValid = buttonsData.some((button) => button.internal === buttonsText);
-  // {isButtonValid ? (
-  //   text='descripcion'
-  // ) : (
-  //   text='descripcion'
-  //   )}
   return (
     <div>
       {/* Render the poem at the beginning */}
@@ -60,7 +56,11 @@ const Informations = ({ json }) => {
 
             <a
               style={{ wordBreak: "break-word" }}
-              href={`https://www.todocoleccion.net/buscador?bu=${encodedProduct}&sec=${buttonsText}&O=menos`}
+              href={
+                isButtonValid
+                  ? `https://www.todocoleccion.net/buscador?bu=${encodedProduct}&sec=${buttonsText}&O=menos`
+                  : `https://www.todocoleccion.net/buscador?bu=${encodedProduct}&O=menos`
+              }
               target="_blank"
               rel="noopener noreferrer"
             >
