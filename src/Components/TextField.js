b0469cf19ef21e91c44a5464a27206bae3126b97
bilalbhatti139 <bilalbhatti139@gmail.com>
2023-12-19 18:24:25 +0500

    err-resolving
    err-resolving

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -237,7 +237,8 @@ Descripción:
 
       // Set the formatted response
       setResponse(formattedResponse);
-      const jsonObject = JSON.parse(openaiResponse);
+      const jsonObject = convertStringToJson(openaiResponse)
+      // const jsonObject = JSON.parse(openaiResponse);
       console.log("jj", jsonObject);
       setResponse(jsonObject);
 
@@ -258,28 +259,98 @@ Descripción:
     const engine = "gpt-3.5-turbo";
     try {
       // Make the initial request to OpenAI API
-      const prompt = `
-      Eres un asesor y tienes que dar recomendaciones, en base a la información que yo te proporcione. A continuación te voy a pasar 5 preguntas con 5 respuestas que ha dado un usuario. En base a esa información, tienes que redactar un pequeño poema, chiste o acertijo, de no más de 8 líneas, y por otro lado, 5 productos. Para cada producto tienes que indicar una breve descripción y el título. Tienes que recomendar obligatoriamente {{category}}.
-      {question-1}:{answer-1}
-      {question-2}:{answer-2}
-      {question-3}:{answer-3}
-      {question-4}:{answer-4}
-      {question-5}:{answer-5}
-      La estructura de tu respuesta tiene que ser un JSON así SIEMPRE. No incluyas nada extra que no esté en esta estructura que te proporciono. Devuelve solo este JSON con los valores correspondientes:
-      {
-           "poema": ...,
-           "titulo1": ...,
-           "producto1": ...,
-           "recomendacion2": ...,
-           ...
-      }
-      ${questions
-        .map(
-          (question, index) =>
-            `${question}:${formData[`question-${index + 1}`]}`
-        )
-        .join("\n")}
-    `;
+      const systemContent =
+      selectedQuestions.length !== 0
+        ? `Eres un asesor con mucho sentido del humor y debes proporcionar recomendaciones basadas en las respuestas del usuario. El usuario es un coleccionista al que le gustan las bromas. A continuación, se presentan preguntas con sus respectivas respuestas. Con base en esta información, crea un pequeño poema, chiste o acertijo (máximo 8 líneas) y recomienda 5 productos concretos de la categoría {{category}}, que se puedan encontrar en todocoleccion. Recuerda que si estamos en  ${buttonsText} SOLO QUIERO LOS TÍTULOS, el autor y una descripción con sentido del humor de al menos 50 palabras. Ni colecciones, ni posters, ni me digas el formato del producto. NO QUIERO NADA QUE NO SEA EL TÍTULO.  Vas a darme las respuesta en formato JSON. La estructura de tu respuesta tiene que ser un JSON  SIEMPRE. No incluyas nada extra que no esté en esta estructura que te proporciono. Devuelve solo este JSON con los valores correspondientes:
+        "poema"
+        "titulo1"
+        "autor1"
+        "descripcion1"
+        "titulo2"
+        "autor2"
+        "descripcion2"
+        "titulo3"
+        "autor3"
+        "descripcion3"
+        "titulo4"
+        "autor4"
+        "descripcion4"
+        "titulo5"
+        "autor5"
+        "descripcion5"
+        Cuando termines de generar el JSON para, no hagas una nueva iteración
+        Preguntas y respuestas:
+        {question-1}:{answer-1}
+        {question-2}:{answer-2}
+        {question-3}:{answer-3}
+        {question-4}:{answer-4}
+        {question-5}:{answer-5}`
+        : `Eres un asesor con mucho sentido del humor y debes proporcionar recomendaciones basadas en las respuestas del usuario. El usuario es un coleccionista al que le gustan las bromas. Genera un JSON con títulos cortos y precisos, de máximo dos palabras, junto con descripciones desenfadadas de por lo menos 50 palabras, para 5 objetos que podría encontrar en todocoleccion, (tienen que ser productos relacionados con arte, antigüedades, coleccionismo, libros,... y cosas que puedan comprarse en todocoleccion,) atendiendo a la descripción proporcionada. Con base en esta información, crea también un pequeño poema, chiste o acertijo (máximo 8 líneas) La estructura de tu respuesta tiene que ser un JSON SIEMPRE. No incluyas nada extra que no esté en esta estructura que te proporciono. Cuando termines de generar el JSON para, no hagas una nueva iteración. NO ME RESPONDAS MÁS DE UNA VEZ. SOLO UN JSON Devuelve solo este JSON con los valores correspondientes:
+"poema"
+"titulo1"
+"descripcion1"
+"titulo2"
+"descripcion2"
+"titulo3"
+"descripcion3"
+"titulo4"
+"descripcion4"
+"titulo5"
+"descripcion5"
+Descripción:
+{answer-1}`;
+      const prompt =
+      selectedQuestions.length !== 0
+        ? 
+        `Eres un asesor con mucho sentido del humor y debes proporcionar recomendaciones basadas en las respuestas del usuario. El usuario es un coleccionista al que le gustan las bromas. A continuación, se presentan preguntas con sus respectivas respuestas. Con base en esta información, crea un pequeño poema, chiste o acertijo (máximo 8 líneas) y recomienda 5 productos concretos de la categoría ${buttonsText}, que se puedan encontrar en todocoleccion. Recuerda que si estamos en  {{category}} SOLO QUIERO LOS TÍTULOS, el autor y una descripción con sentido del humor de al menos 50 palabras. Ni colecciones, ni posters, ni me digas el formato del producto. NO QUIERO NADA QUE NO SEA EL TÍTULO.  Vas a darme las respuesta en formato JSON. La estructura de tu respuesta tiene que ser un JSON  SIEMPRE. No incluyas nada extra que no esté en esta estructura que te proporciono. Devuelve solo este JSON con los valores correspondientes:
+        "poema"
+        "titulo1"
+        "autor1"
+        "descripcion1"
+        "titulo2"
+        "autor2"
+        "descripcion2"
+        "titulo3"
+        "autor3"
+        "descripcion3"
+        "titulo4"
+        "autor4"
+        "descripcion4"
+        "titulo5"
+        "autor5"
+        "descripcion5"
+        Cuando termines de generar el JSON para, no hagas una nueva iteración
+        Preguntas y respuestas:
+        {question-1}:{answer-1}
+        {question-2}:{answer-2}
+        {question-3}:{answer-3}
+        {question-4}:{answer-4}
+        {question-5}:{answer-5}
+  
+    ${questions
+      .map(
+        (question, index) => `${question}:${formData[`question-${index + 1}`]}`
+      )
+      .join("\n")}
+  `
+        : `Eres un asesor con mucho sentido del humor y debes proporcionar recomendaciones basadas en las respuestas del usuario. El usuario es un coleccionista al que le gustan las bromas. Genera un JSON con títulos cortos y precisos, de máximo dos palabras, junto con descripciones desenfadadas de por lo menos 50 palabras, para 5 objetos que podría encontrar en todocoleccion, (tienen que ser productos relacionados con arte, antigüedades, coleccionismo, libros,... y cosas que puedan comprarse en todocoleccion,) atendiendo a la descripción proporcionada. Con base en esta información, crea también un pequeño poema, chiste o acertijo (máximo 8 líneas) La estructura de tu respuesta tiene que ser un JSON SIEMPRE. No incluyas nada extra que no esté en esta estructura que te proporciono. Cuando termines de generar el JSON para, no hagas una nueva iteración. NO ME RESPONDAS MÁS DE UNA VEZ. SOLO UN JSON Devuelve solo este JSON con los valores correspondientes:
+        "poema"
+        "titulo1"
+        "descripcion1"
+        "titulo2"
+        "descripcion2"
+        "titulo3"
+        "descripcion3"
+        "titulo4"
+        "descripcion4"
+        "titulo5"
+        "descripcion5"
+        Descripción:
+        {answer-1}
+        ${questions
+          .map((index) => `${formData[`question-${index + 1}`]}`)
+          .join("\n")}`
+     ;
 
       try {
         // Make a request to the OpenAI API
@@ -290,21 +361,7 @@ Descripción:
             messages: [
               {
                 role: "system",
-                content: `Eres un asesor y tienes que dar recomendaciones, en base a la información que yo te proporcione.
-              user content: A continuación te voy a pasar 5 preguntas con 5 respuestas que ha dado un usuario. En base a esa información, tienes que redactar un pequeño poema, chiste o acertijo, de no más de 8 líneas, y por otro lado, 5 productos. Para cada producto tienes que indicar una breve descripción y el título. Tienes que recomendar obligatoriamente ${buttonsText}.
-              {question-1}:{answer-1}
-              {question-2}:{answer-2}
-              {question-3}:{answer-3}
-              {question-4}:{answer-4}
-              {question-5}:{answer-5}
-              La estructura de tu respuesta tiene que ser un JSON así SIEMPRE. No incluyas nada extra que no esté en esta estructura que te proporciono. Devuelve solo este JSON con los valores correspondientes:
-              {
-                   "poema": ...,
-                   "titulo1": ...,
-                   "descripcion1": ...,
-                   "titulo2": ...,
-                   ...
-              }`,
+                content:systemContent,
               },
               {
                 role: "user",
@@ -332,7 +389,8 @@ Descripción:
 
         // Set the formatted response
         setResponse(formattedResponse);
-        const jsonObject = JSON.parse(openaiResponse);
+        const jsonObject=convertStringToJson(openaiResponse);
+        // const jsonObject = JSON.parse(openaiResponse);
         console.log("jj", jsonObject);
         setResponse(jsonObject);
 
